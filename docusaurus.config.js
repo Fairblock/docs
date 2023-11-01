d2830e60c6c02576809eb386bed3f914764c124a
bullishgopher <49041460+bullishgopher@users.noreply.github.com>
2023-11-01 06:10:14 -0700

    feat: add frontend integration docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,15 +12,15 @@ const config = {
   favicon: 'img/favicon/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.fairblock.network',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
